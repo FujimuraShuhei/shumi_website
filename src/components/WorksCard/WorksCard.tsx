b51086774c090acd68c3b8574ff9aa9481f11376
shuhei_fujimura <shushu2525.1694@gmail.com>
2023-02-09 20:18:50 +0900

    WorksCardリファクタ

--- src/components/WorksCard/WorksCard.tsx
+++ src/components/WorksCard/WorksCard.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { Container } from '@mantine/core';
 import WorksCardParts from '../../hooks/Works/WorksCardParts';
 import WorksTitle from '../../hooks/Works/WorksTitle';
+import { motion } from 'framer-motion';
 
 const WorksCard = () => {
   return (
