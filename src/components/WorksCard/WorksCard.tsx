b92bf2fafcbb577b71ec5dc02497ff488df20362
shuhei_fujimura <shushu2525.1694@gmail.com>
2023-02-09 19:51:45 +0900

    WorksCardリファクタリング

--- src/components/WorksCard/WorksCard.tsx
+++ src/components/WorksCard/WorksCard.tsx
@@ -1,42 +1,12 @@
 import React, { useEffect } from 'react';
-import { Carousel } from '@mantine/carousel';
-import Image from 'next/image';
-import works1 from '/public/images/works1.jpg';
 import { Container } from '@mantine/core';
-import { motion, useAnimationControls } from 'framer-motion';
-import styles from './WorksCard.module.css';
 import WorksCardParts from '../../hooks/Works/WorksCardParts';
+import WorksTitle from '../../hooks/Works/WorksTitle';
 
 const WorksCard = () => {
-  const controls = useAnimationControls();
-  const cardFade = useAnimationControls();
-
-  useEffect(() => {
-    controls.start({
-      width: ['0%', '100%'],
-      overflow: 'hidden',
-      opacity: [0, 100],
-    });
-  }, [controls]);
-
-  useEffect(() => {
-    cardFade.start({
-      type: 'spring',
-      transition: { duration: 3 },
-    });
-  }, [cardFade]);
-
   return (
     <Container size="lg">
-      <div className="mt-20 mb-10">
-        <motion.h2
-          animate={controls}
-          transition={{ duration: 2 }}
-          className={`"text-7xl text-slate-900 italic mb-6" ${styles.flowing}`}>
-          Works
-        </motion.h2>
-        <p className="text-sm">制作実績</p>
-      </div>
+      <WorksTitle />
       <WorksCardParts />
     </Container>
   );
---
+++ src/hooks/Works/WorksTitle.tsx
@@ -0,0 +1,28 @@
+import { motion, useAnimationControls } from 'framer-motion';
+import React, { useEffect } from 'react';
+import styles from './WorksCard.module.css';
+
+const WorksTitle = () => {
+  const controls = useAnimationControls();
+
+  useEffect(() => {
+    controls.start({
+      width: ['0%', '100%'],
+      overflow: 'hidden',
+      opacity: [0, 100],
+    });
+  }, [controls]);
+  return (
+    <div className="mt-20 mb-10">
+      <motion.h2
+        animate={controls}
+        transition={{ duration: 2 }}
+        className={`"text-7xl text-slate-900 italic mb-6" ${styles.flowing}`}>
+        Works
+      </motion.h2>
+      <p className="text-sm">制作実績</p>
+    </div>
+  );
+};
+
+export default WorksTitle;
